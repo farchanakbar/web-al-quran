757010e396190ec95e3266fd05c3c286c4f92ed8
Farchan Akbar <farchanakbar80@gmail.com>
2023-06-13 06:52:37 +0700

    update

--- src/components/SurahDetail.jsx
+++ src/components/SurahDetail.jsx
@@ -74,8 +74,8 @@ function SurahDetail() {
                 </button>
               )}
             </div>
-            <h1 className='text-white text-3xl md:text-5xl leading-relaxed text-end font-arab mt-5'>{item.text?.arab}</h1>
-            <p className='text-white mt-5'>{item.text?.transliteration?.en}</p>
+            <h1 className='text-white text-4xl sm:text-5xl leading-relaxed text-end font-arab mt-5'>{item.text?.arab}</h1>
+            <p className='text-white mt-5 md:mt-10 '>{item.text?.transliteration?.en}</p>
             <p className='text-white mt-5'>Artinya : {item.translation?.id}</p>
           </li>
         ))}
